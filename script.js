d12d3b131edae2f3c879ebdd0f75302dd875da29
user <caid@bxscience.edu>
2022-03-15 01:11:29 -0400

    Added delete functions

--- script.js
+++ script.js
@@ -66,13 +66,44 @@ function addC() {
 
 //Removes a row
 function removeR() {
-    //document.getElementById("grid").deleteRow(0);
-    alert("Clicked Remove Row")
+
+    //stops if the table is empty
+    let grid = document.getElementById("grid");
+    let rows = document.getElementsByTagName("tr");
+    if (rows.length === 1) {
+        grid.innerHTML = "";
+    }
+    if (rows.length === 0) {
+        return;
+    }
+
+    //deletes the last row since -1 is the last of the index
+    document.getElementById("grid").deleteRow(-1);
+
 }
 //Remove a column
 function removeC() {
-    alert("Clicked Remove Col")
-}
+
+    //stops if the table is empty
+    let grid = document.getElementById("grid");
+    let col = document.getElementsByTagName("td");
+    if (col.length === 1) {
+        grid.innerHTML = "";
+    }
+    if (col.length === 0) {
+        return;
+    }
+    //just gonna inverse what i did earlier for adding cols
+    let rows = document.getElementsByTagName("tr");
+
+    let cols = grid.rows[0].cells.length;
+	    let deletedcell;
+	    for(var i=0;i<grid.rows.length;i++){
+		    deletedcell = grid.rows[i].deleteCell(-1);
+    	}
+    }
+
+
 //sets global var for selected color
 function selected(){
     colorSelected = document.getElementById("selectedID").value;
